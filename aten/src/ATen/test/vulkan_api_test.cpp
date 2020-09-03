dfcbf28fb50f4fe6102dc8f06fe69088a80764ca
Ashkan Aliabadi <ashkanaliabadi@fb.com>
2020-09-02 21:02:19 -0700

    Add Vulkan Tensor factory.
    ghstack-source-id: bb073e6a7be5f613f613feee84bc26a47abad039
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/44016

--- aten/src/ATen/test/vulkan_api_test.cpp
+++ aten/src/ATen/test/vulkan_api_test.cpp
@@ -3,6 +3,7 @@
 #ifdef USE_VULKAN_API
 
 #include <ATen/native/vulkan/api/api.h>
+#include <test/cpp/jit/test_utils.h>
 
 namespace {
 
@@ -13,6 +14,14 @@ TEST(VulkanAPITest, Runtime) {
 
 TEST(VulkanAPITest, Context) {
   ASSERT_NO_THROW(at::native::vulkan::api::context());
+}
+
+TEST(VulkanAPITest, empty) {
+  if (!at::native::vulkan::api::available()) {
+    return;
+  }
+
+  ASSERT_NO_THROW(at::empty({1, 3, 64, 64}, at::device(at::kVulkan).dtype(at::kFloat)));
 }
 
 } // namespace
